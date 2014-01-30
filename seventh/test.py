ae0e764222212a931f60eebb5b5938dace7de8aa
Talat Parwez <stalat@zeomega.com>
2014-01-31 03:06:31 +0530

    Test Cases with code coverage

--- seventh/test.py
+++ seventh/test.py
@@ -1,23 +1,23 @@
+import unittest
 from add import add
-def test_add_two_numbers():
-	result=add(10,1)
-	print result
-	assert 11==result
-	print "Test Case 1 is executed successfully"
+class TestMath(unittest.TestCase):
+	def test_add_two_numbers():
+		result=add(10,1)
+		print result
+		assert 11==result
+		print "Test Case 1 is executed successfully"
 
-def test_add_strings():
-	result=add("User","Case")
-	print result
-	assert "UserCase"==result
-	print "Test Case 2 is executed successfully"
+	def test_add_strings():
+		result=add("User","Case")
+		print result
+		assert "UserCase"==result
+		print "Test Case 2 is executed successfully"
 
-def test_add_lists():
-	result=add(["Hello"],["Talat"])
-	print result
-	assert ['Hello','Talat']==result
-	print "Test Case 3 is executed successfully"
+	def test_add_lists():
+		result=add(["Hello"],["Talat"])
+		print result
+		assert ['Hello','Talat']==result
+		print "Test Case 3 is executed successfully"
 
-if __name__ == '__main__':
-	test_add_two_numbers()	
-	test_add_strings()
-	test_add_lists()
+	if __name__ == '__main__':
+		unittest.main()
